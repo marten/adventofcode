191a770efcb3fe82c6a21bd82bc42ca372cc5f05
Marten Veldthuis <marten@veldthuis.com>
2017-12-06 15:04:27 +0000

    Add 6ab

---
+++ 2017/6-ruby/6a.rb
@@ -0,0 +1,22 @@
+require 'set'
+
+state = ARGF.read.split(/\s+/).map(&:to_i)
+states = Set.new
+history = []
+
+while !states.include?(state)
+  states << state
+  history << state.dup
+
+
+  max = state.max
+  idx = state.index(max)
+  state[idx] = 0
+  for i in 1..max
+    state[(idx + i) % state.size] += 1
+  end
+  # puts state.inspect
+end
+
+puts "Iterations needed: #{states.size}"
+puts "Loop size: #{states.size - history.index(state)}"
